584ccac1b814a237d3f224fd2707e1e4977400ab
ANUGEETHA03 <55158625+ANUGEETHA03@users.noreply.github.com>
2020-01-20 19:27:16 -0500

    WebPage Creation

---
+++ Table.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from netmiko import ConnectHandler
+import json
+from prettytable import PrettyTable 
+from flask import Flask,render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    
+    with open("NSOT_OoB.json") as fo:
+      	oob = json.load(fo)
+        
+    with open("NSOT_inband.json") as  fi:\
+            inband = json.load(fi)
+
+    rw = ["Devices"] 
+    net_stat = {}
+    for device in oob.keys():
+        rw.append(device)
+        ping_stat = [] 
+        ping_stat.append(device)
+        net_connect = ConnectHandler(**oob[device])
+        print("Trying to SSH into the Device " + device)
+        print("Initiating Ping Sequence for device " + device) 
+        for i in inband.keys():
+            pi = 1 
+            for j in inband[i]: 
+                dev  = inband[i][j]['ip']
+                print("Trying to ping " + dev)
+                res = net_connect.send_command("ping " + dev + " repeat 3")
+                if('round-trip' in res):
+                    res = 1 
+                else: 
+                     res = 0
+                pi = int(pi and res)
+                print(pi)
+            ping_stat.append(pi)    
+            
+        net_stat.update({device : ping_stat})
+        print(net_stat)
+    
+        print("Disconnecting from device " + device)
+        net_connect.disconnect()
+
+    table = PrettyTable(rw)
+    for k in net_stat.keys():
+       table.add_row(net_stat[k])
+
+    print("Connectivity Check Done")
+    print("Status Generated")
+    print(type(table))
+    p_table = table.get_html_string()
+    return(p_table)
+
+
+if __name__ == '__main__': 
+       app.debug = True 
+       app.run(host = '192.168.44.159', port = 80) 
